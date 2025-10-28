a407d826cba2f51ea61b8ba506353682563a1745
MisaelZanetti <misaelzanetti@gmail.com>
2025-10-28 10:49:39 -0300

    Primera actividad

--- src/Productos.jsx
+++ src/Productos.jsx
@@ -1,23 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Productos.css";
 
 export default function Productos() {
   const [productos, setProductos] = useState([]);
 
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setProductos(data))
+      .catch(() => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Mala conexión de wifi!"
+        });
+      });
+  }, []);
 
   return (
     <div className="productos-container">
       <h2 className="productos-title">Lista de productos</h2>
       <div className="productos-grid">
-        {/* // Estructura sugerida para mostrar cada producto:
-        // <div className="producto">
-        //   <img src={producto.image} alt={producto.title} />
-        //   <h3>{producto.title}</h3>
-        //   <p className="categoria">Categoría: {producto.category}</p>
-        //   <p className="precio">${producto.price}</p>
-        //   <p className="rating">⭐ {producto.rating.rate} ({producto.rating.count} reseñas)</p>        
-        // </div> */}
-
+        {productos.map((producto) => (
+          <div className="producto-card" key={producto.id}>
+            <img src={producto.image} alt={producto.title} className="producto-img" />
+            <h3 className="producto-titulo">{producto.title}</h3>
+            <p className="producto-categoria">Categoría: {producto.category}</p>
+            <p className="producto-precio">${producto.price}</p>
+            <p className="producto-rating">
+              ⭐ {producto.rating.rate} ({producto.rating.count} reseñas)
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
